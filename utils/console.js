78aaaae1d033aad99e993c32ae39e9c5193b0f8b
Death死神 <76556553+YukihiraX@users.noreply.github.com>
2023-07-10 12:24:30 +0400

    console.js

--- utils/console.js
+++ utils/console.js
@@ -1,21 +1,21 @@
 import chalk from 'chalk';
 const init = async () => {
     console.log(chalk.yellow.bold(`
-        /$$       /$$                              
-        | $$      |__/                              
-        | $$       /$$ /$$   /$$  /$$$$$$   /$$$$$$ 
-        | $$      | $$| $$  | $$ /$$__  $$ |____  $$
-        | $$      | $$| $$  | $$| $$  \ $$  /$$$$$$$
-        | $$      | $$| $$  | $$| $$  | $$ /$$__  $$
-        | $$$$$$$$| $$|  $$$$$$/|  $$$$$$$|  $$$$$$$
-        |________/|__/ \______/  \____  $$ \_______/
-                                    | $$          
-                                    | $$          
-                                    |__/
+| $$ |          |$$|                  |$$|                  |$$|              |$$|         |$$|
+| $$ |        |$$|                 |$$|  |$$|               |$$$$|          |$$$$|         |$$|
+| $$ |      |$$|                 |$$|      |$$|             |$$|$$|        |$$|$$|         |$$|
+| $$ |    |$$|                  |$$|        |$$|            |$$||$$|      |$$||$$|         |$$| 
+| $$ |  |$$|                   |$$|         |$$|            |$$| |$$|    |$$| |$$|         |$$|  
+| $$ |$$|                      |$$|         |$$|            |$$|  |$$|  |$$|  |$$|         |$$|    
+| $$ |  |$$|                   |$$|         |$$|            |$$|   |$$||$$|   |$$|         |$$|     
+| $$ |    |$$|                 |$$$$$$$$$$$$$$$|            |$$|    |$$$$|    |$$|         |$$|     
+| $$ |      |$$|               |$$|         |$$|            |$$|              |$$|         |$$|     
+| $$ |        |$$|             |$$|         |$$|            |$$|              |$$|         |$$|  
+| $$ |          |$$|           |$$|         |$$|            |$$|              |$$|         |$$| 
+
     `))
-    console.log(chalk.green.bold('                  Aile Sorgulama Sistemi'));
-    console.log(chalk.red.bold('Uygulamadan doğacak herhangi bir sorumluluk kabul edilmemektedir.'));
-    console.log(chalk.blue.bold(`               Uygulama geliştiricisi: @Furkanisyanedior`));
+    console.log(chalk.green.bold('                 https://discord.gg/zKpsrghxrU'));
+    console.log(chalk.blue.bold(`               Uygulama geliştiricisi: @onlyrespect`));
 }
 
 export default {
